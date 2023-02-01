b354888dbda78e836b6939596408c277b102d036
h-tawfik <hadeel@kobotoolbox.org>
2023-02-01 10:42:13 +0000

    commenst code

--- GeoNode/Controllers/GeonodeController.cs
+++ GeoNode/Controllers/GeonodeController.cs
@@ -13,21 +13,28 @@ namespace GeoNode.Controllers
     public class GeonodeController : ControllerBase
     {
         [HttpPost]
+        // This is a method for handling HTTP POST requests
+
         public async Task<IActionResult> Post([FromHeader(Name = "geo_api_url")] string geoNodeApiUrl,
             [FromHeader(Name = "geo-layer")] string geoLayer,
             [FromBody] JsonElement data)
         {
             try
             {
+                // Get the form data from the input JSON data by using helper
                 var formData = FormDataHelper.GetFormData(data);
 
+                // Create an HTTP client instance
                 using var http = new HttpClient();
-
+               
+                // Create a new HTTP POST request using the geoNodeApiUrl
                 var request = new HttpRequestMessage(HttpMethod.Post, geoNodeApiUrl);
 
+                // Get the base URI from the geoNodeApiUrl
                 var uri = new Uri(geoNodeApiUrl);
                 var baseUri = uri.GetLeftPart(System.UriPartial.Authority);
-
+                
+                // Define the XML request dynamic body
                 var body =
                   @"<wfs:Transaction service=""WFS"" version=""1.1.0"" xmlns:wfs=""http://www.opengis.net/wfs"" xmlns:gml=""http://www.opengis.net/gml"" xmlns:ogc=""http://www.opengis.net/ogc"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xsi:schemaLocation=""http://www.opengis.net/wfs"" xmlns:geonode=""{4}"">
                     <wfs:Insert>
@@ -42,22 +49,33 @@ namespace GeoNode.Controllers
                     </wfs:Insert>
                 </wfs:Transaction>";
 
+                // Get the server URL from the base URI
                 var ServerURL = baseUri?.Substring(0, baseUri.LastIndexOf(":")) + "/";
 
+                // Format the XML body with the form data and server URL
                 var bbody = string.Format(body, formData.Longitude, formData.Latitude, formData.GeoNodeAttributes, geoLayer, ServerURL);
+
+                // Set the content of the request to the formatted XML body
                 request.Content = new StringContent(bbody, Encoding.UTF8, "application/xml");
 
+                // Send the request to the geoNode API
                 var res = await http.SendAsync(request);
 
                 // return the response from the geoserver api
                 var rescont = await res.Content.ReadAsStringAsync();
+                
+                // Return an error status code and the response if the request was not successful
                 if (!res.IsSuccessStatusCode)
                 {
                     return StatusCode((int)res.StatusCode, rescont);
                 }
+
+                // Return an OK status code if the request was successful
                 return Ok();
             }
-            catch (Exception ex)
+
+            // Return a Bad Request status code and the exception message if an error occurs
+           catch (Exception ex)
             {
                 return StatusCode(400, ex.Message);
             }
--- GeoNode/Helpers/GetFormDataHelper.cs
+++ GeoNode/Helpers/GetFormDataHelper.cs
@@ -7,32 +7,53 @@ using System.Text.Json;
     {
     public class FormDataHelper
     {
+        // This method parses JSON data from a JsonElement object and returns a FormDataModel object
+
         public static FormDataModel GetFormData(JsonElement dataElement)
         {
+            // Initialize a new FormDataModel object
             var formData = new FormDataModel();
-            //var dataElement = data.GetProperty("data");
+
+            // Get the "_geolocation" property from the JsonElement object
             var _geolocation = dataElement.GetProperty("_geolocation").EnumerateArray();
+            // Set the Latitude and Longitude properties of the FormDataModel object
             formData.Latitude = float.Parse(_geolocation.First().ToString());
             formData.Longitude = float.Parse(_geolocation.Last().ToString());
 
+            // Get all properties from the JsonElement object as an enumerable collection of JsonProperty objects
             var props = dataElement.EnumerateObject();
+            // Filter out the "_geolocation" property and convert the resulting list to a list of JsonProperty objects
             var attrs = props.Where(s => !s.Name.Contains("_geolocation")).ToList();
+            // Iterate over the list of remaining properties
+
             foreach (var attr in attrs)
             {
+                // Get the attribute name after it has been processed by the AttrNameHandler method
+
                 var attrName = AttrNameHandler(attr.Name);
+                // Add the attribute name and its value to the Attributes dictionary of the FormDataModel object
+
                 formData.Attributes.Add(attrName, attr.Value.ToString());
             }
+            // Return the FormDataModel object
+
             return formData;
         }
 
+        // This method handles the processing of attribute names
+
         private static string AttrNameHandler(string name)
         {
+            // Initialize a newName variable with the original name
+
             string newName = name;
+            // If the name contains a "/" character, replace it with "__" (this especially for grouping)
+
             if (name.Contains("/"))
             {
                 newName = name.Replace("/", "__");
             }
-
+            // Return the new name in all lowercase
             return newName.ToLower();
         }
     }
